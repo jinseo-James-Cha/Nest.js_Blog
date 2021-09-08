7760c3b6f2c960430273055a36928e817ae6671b
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-09-08 17:31:10 +0900

    2021-09-08 config folder created to save typeorm config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
+import { TypeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), BoardsModule],
+  imports: [
+    TypeOrmModule.forRoot(TypeORMConfig),
+    BoardsModule
+  ],
   controllers: [],
   providers: [],
 })
