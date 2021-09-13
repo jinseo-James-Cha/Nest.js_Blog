c4dbeb991ff45e77f15ddb49bd0c739c93801a61
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-09-13 17:24:59 +0900

    2021-09-13 DI done in controller and service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserRepository } from './user.repository';
 
 @Module({
+  // eslint-disable-next-line prettier/prettier
+  imports: [
+    TypeOrmModule.forFeature([UserRepository])
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
