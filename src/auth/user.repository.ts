3feb212cf706903c95f3817f3646136f9801f93e
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-09-13 18:06:31 +0900

    2021-09-13 signup done

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,13 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(AuthCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = AuthCredentialsDto;
+    const user = this.create({ username, password });
+
+    await this.save(user);
+  }
+}
