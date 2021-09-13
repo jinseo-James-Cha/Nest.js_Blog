2c16e43bc1ab4c4469a045d64ac0ac803df5f5fb
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-09-14 00:35:42 +0900

    2021-09-14 bcryptjs done, checked in db as OK

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,12 +5,18 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(AuthCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = AuthCredentialsDto;
-    const user = this.create({ username, password });
+
+    // bcrypt password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
