e034959f15bce6642922b708efbf9b80b0d46037
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-09-22 01:04:53 +0900

    2021-09-22 main.ts edited with config file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config'; // 1
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+
+  const serverConfig = config.get('server'); // 2
+  const port = serverConfig.port; // 3
 
   // Global Pipe
   app.useGlobalPipes(
@@ -15,7 +19,7 @@ async function bootstrap() {
     }),
   );
 
-  const port = 3000;
+  // const port = 3000;
   await app.listen(port);
 
   Logger.log(`Application running on port ${port}`);
