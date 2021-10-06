c80f6f50cea3fb509b7b88b61e8227b4739669bf
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-10-06 15:53:48 +0900

    2021-10-06 caching interceptor done

--- src/interceptors/caching.interceptor.ts
+++ src/interceptors/caching.interceptor.ts
@@ -4,11 +4,16 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class CachingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const isCached = true;
+    if (isCached) {
+      return of([]);
+    }
+
     return next.handle();
   }
 }
