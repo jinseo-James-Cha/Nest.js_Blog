2de329beb103bdfc4b5f4fa9ff3b92a184a3d828
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-10-07 12:30:32 +0900

    2021-10-07 timeout error testing refecting

--- src/interceptors/timeout.interceptor.ts
+++ src/interceptors/timeout.interceptor.ts
@@ -17,15 +17,12 @@ import {
 @Injectable()
 export class TimeoutInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('1');
     return next.handle().pipe(
       timeout(3000),
       catchError((err) => {
         if (err instanceof TimeoutError) {
-          console.log('interceptor');
           return throwError(() => new RequestTimeoutException());
         }
-        console.log('2');
         return throwError(() => err);
       }),
     );
