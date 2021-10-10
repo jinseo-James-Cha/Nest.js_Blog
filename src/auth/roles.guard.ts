fd2b9a4d3cb27a3dd2e9d70be9e3a43baf5a06b3
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-10-11 04:27:49 +0900

    2021-10-11 code refactoring - 2

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -17,8 +17,6 @@ export class RolesGuard implements CanActivate {
     }
     const { user } = context.switchToHttp().getRequest();
 
-    console.log('requiredRoles', requiredRoles);
-    console.log('user', user);
     return requiredRoles.some((role) => user.role?.includes(role));
   }
 }
